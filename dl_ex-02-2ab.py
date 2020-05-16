6a3d8ed508e04b4537410694a1423e9346a57046
Jodscho <Jonathan2008@gmx.de>
2020-05-16 21:45:10 +0200

    ex-03: xaviar-initalization and rms-prop

--- dl_ex-02-2ab.py
+++ dl_ex-02-2ab.py
@@ -1,12 +1,25 @@
 import random
+from numpy import random as r
+import math
+import matplotlib.pyplot as plt
+
 
 def relu(value): return 0 if value < 0 else value
 
 def relu_der(value): return 0 if value <= 0 else 1 
 
-def loss(y, y_hat):	return (y - y_hat)**2
+def loss(y, y_hat):
+	loss = 0
+	for i in range(len(y_hat)):
+		loss += (y[i] - y_hat[i])**2
+	return loss
 
-def loss_der(y, y_hat):	return -2 * (y - y_hat)
+
+def loss_der(y, y_hat):
+	loss = 0
+	for i in range(len(y_hat)):
+		loss += -2 * (y[i] - y_hat[i])
+	return loss
 
 def init_array(i, j = 0):
 	if j is 0:
@@ -17,32 +30,140 @@ def init_array(i, j = 0):
 def nodes_to_drop_out(layer_size):
 	return [random.uniform(0, 1) > 0.8 for i in range(layer_size)]
 
-def stochastic_gd(w, v, gradient_v, gradient_w, eta):
+def	constant_initalization(val):
+	v = []
+	for i in range(2): v.append(val)
+	v.append(0) # for the bias
 
-	print("---- START: SGD ----")
+	w = []
+	for i in range(2):
+		w.append([])
+		for j in range(2):
+			w[i].append(val)
+		w[i].append(0) # for the bias
 
-	# update the v values
-	v_new = init_array(len(v))
-	for i in range(len(v)):
-		v_new[i] = v[i] - eta * gradient_v[i]
-		print("v_new_{} = {} - {} * {} = {}")\
-		.format(i, v[i], eta, gradient_v[i], v_new[i])
+	return w, v
 
-	# update the w values
-	w_new = init_array(len(w), len(w[0]))
-	for i in range(len(w)):
-		for j in range(len(w[0])):
-			w_new[i][j] = w[i][j] - eta * gradient_w[i][j]
-			print("w_new_{}{} = {} - {} * {} = {}")\
-			.format(i, j, w[i][j], eta, gradient_w[i][j], w_new[i][j])
+def xaviar_initalization():
+	# pick from the following gaussian distribution: 
+	# N~(0, 1/N)
 
-	print("---- END: SGD ----")
+	# for v, (2+1)/2 = 1.5, 1/1.5 = 0.6666
+	v = []
+	for i in range(2):
+		v.append(r.normal(loc=0, scale=0.666))
+	v.append(0) # for the bias
 
-	return v_new, w_new
+	# for w, (2+2)/2 = 2, 1/2 = 0.5
+	w = []
+	for i in range(2):
+		w.append([])
+		for j in range(2):
+			w[i].append(r.normal(loc=0, scale=1/0.5))
+		w[i].append(0) # for the bias
+
+	return w, v
+
+def rms_prop(w, v, dataset, delta, roh, iterations):
+
+	x_axis = []
+	y_axis = []
+
+	r_v = init_array(len(v))
+	r_w = init_array(len(w), len(w[0]))
+	mu = 0.1 # no mu given in task description
+
+	for epoch in range(iterations):
+		x_axis.append(epoch)
+
+		gradient_w, gradient_v = backpropagation_dataset(v, w, dataset, y_axis)
+
+		for i in range(len(v)):
+			r_v[i] = roh * r_v[i] + (1 - roh)*(v[i] * v[i])
+			mu_tilde = mu / (delta + math.sqrt(r_v[i]))
+			v[i] = v[i] - mu_tilde * gradient_v[i]
+
+		for i in range(len(w)):
+			for j in range(len(w[0])):
+				r_w[i][j] = roh * r_w[i][j] + (1 - roh)*(w[i][j] * w[i][j])
+				mu_tilde = mu / (delta + math.sqrt(r_w[i][j]))
+				w[i][j] = w[i][j] - mu_tilde * gradient_w[i][j]
+		
+	#return v, w
+
+	print("v = {}").format(v)
+	print("w = {}").format(w)
+	return x_axis, y_axis
+
+
+def backpropagation_dataset(v, w, dataset, y_axis):
+	"""Calculates the gradients over the entire training set.
+	"""
+
+	y = [i[2] for i in dataset] # get all the y-values from the dataset as a vector
+	y_hat_all = forward_computation_dataset(w, v, dataset) # compute prediction for every input
+	y_axis.append(loss(y, y_hat_all))
+
+	print("total loss: {}").format(loss(y, y_hat_all))
+
+	# calculate the derivative loss for the backpropagation
+	loss_der_now = loss_der(y, y_hat_all)
+
+	# contains the gradients over all training elements
+	gradient_v_all = init_array(len(v))
+	gradient_w_all = init_array(len(w), len(w[0]))
+
+	# call the backpropagation method for every element in the training set
+	for i in range(len(dataset)):
+		# get the current x from the dataset
+		x = dataset[i]
+		x[len(x)-1] = 1 # override y to be the bias 1
+
+		# we need the activation output for backprop
+		h_l1 = forward_computation(w, v, x)[0]
+
+		# compute gradients for this training element
+		gr_v, gr_w = backpropagation(v, w, x, h_l1, loss_der_now)
+
+		# add gradient values to the total sum of gradient values
+		for j in range(len(gr_v)):
+			gradient_v_all[j] += gr_v[j]
+
+		# same for w
+		for j in range(len(w)):
+			for k in range(len(w[0])):
+				gradient_w_all[j][k] += gr_w[j][k]
+
+	# now average the gradient over the number of training examples
+	for j in range(len(gr_v)):
+		gradient_v_all[j] /= len(dataset)
+
+	for j in range(len(w)):
+		for k in range(len(w[0])):
+			gradient_w_all[j][k] /= len(dataset)
+
+	print("gradient_w_all: {}").format(gradient_w_all)
+	print("gradient_v_all: {}").format(gradient_v_all)
+
+	return gradient_w_all, gradient_v_all
+
+def forward_computation_dataset(w, v, dataset):
+	"""Calculates the predictions for the entire training set.
+	"""
+
+	y_hat_all = []
+
+	for i in range(len(dataset)):
+		x = dataset[i]
+		x[len(x)-1] = 1 # override y to be the bias 1
+		y_hat_all.append(forward_computation(w, v, x)[1])
+
+	return y_hat_all
+
 
 def forward_computation(w, v, x):
-
-	print("---- START: FORWARD COMPUTATION ----")
+	"""Calculates the prediction for a specific input.
+	"""
 
 	# first compute the result for the first layer
 	a_l1 = init_array(len(w))
@@ -55,114 +176,71 @@ def forward_computation(w, v, x):
 	a_l1.append(1)  # for the bias
 	h_l1 = [relu(element) for element in a_l1]
 
-	print("a_layer_1 = {}").format(a_l1)
-	print("h_layer_1 = {}").format(h_l1)
-
 	# then compute the result for the second layer
 	y_hat = 0
 	for i in range(len(v)):
 		y_hat += h_l1[i] * v[i]
 
-	print("y_hat = {}").format(y_hat)
-
-	print("---- END: FORWARD COMPUTATION ----")
-
 	return h_l1, y_hat
 
 
-def backpropagation(x, v, w, h_l1, y_hat, y, dropped_nodes):
 
-	print("---- START: BACKPROPAGATION ----")
+def backpropagation(v, w, x, h_l1, loss_der_now, dropped_nodes = []):
+	"""Execute backpropgation for one training example.
+	Note: Because the result is only the gradient for one training example
+	one has to call this method for every training example in the training dataset.
+	"""
 
 	# first calculate the gradient for v
 	gradient_v = init_array(len(v))
 	for i in range(len(v)):
 		# ignore all connections to end node from here, if node is dropped
 		if dropped_nodes and dropped_nodes[i]:
-			print("gradient of v_{} = 0 (dropped)").format(i)
 			continue
-		gradient_v[i] = loss_der(y, y_hat) * h_l1[i]
-		print("gradient of v_{} = {} * {} = {}").\
-		format(i, loss_der(y, y_hat), h_l1[i], gradient_v[i])
+		gradient_v[i] = loss_der_now * h_l1[i]
 
 	# then calculate the gradient for w
 	gradient_w = init_array(len(w), len(w[0]))
 	for i in range(len(w)):
 		# ignore all connections to this node if it is dropped
 		if dropped_nodes and dropped_nodes[i]:
-			print("gradient of w_{}j = 0 for j = 0,...,{} (dropped)").format(i, len(w[0])-1)
 			continue
 		for j in range(len(w[0])):
-			gradient_w[i][j] = x[j] * relu_der(h_l1[i]) * v[i] * loss_der(y, y_hat)
-			print("gradient of w_{}{} = {} * {} * {} {} = {}").\
-			format(i, j, x[j], relu_der(h_l1[i]), v[i], loss_der(y, y_hat), gradient_w[i][j])
+			gradient_w[i][j] = x[j] * relu_der(h_l1[i]) * v[i] * loss_der_now
 
-	print("---- END: BACKPROPAGATION ----")
 
 	return gradient_v, gradient_w
 
 
-def iteration_step(w, v, x, y, eta, use_dropout, dropped_nodes = []):
-
-	# start by computating the activation function output for layer 1
-	# and the output y_hat at the output node
-	h_l1, y_hat = forward_computation(w, v, x)
-
-	loss_val = loss(y, y_hat)
-
-	if use_dropout:
-
-		if not dropped_nodes:
-			# if the user did not provide dropped nodes, drop randomly
-			dropped_nodes = nodes_to_drop_out(len(w))
-		
-		print("dropped nodes: {}").format(dropped_nodes)
-		dropped_nodes.append(False) # one more for the second-layer-bias which is never dropped
-
-	gradient_v, gradient_w = backpropagation(x, v, w, h_l1, y_hat, y, dropped_nodes)
-
-	v_new, w_new = stochastic_gd(w, v, gradient_v, gradient_w, eta)
-
-	return v_new, w_new, y_hat
-
 def main():
 
-	w = [[-1, -2, 1], [-3, 2, 2], [1, 3, 1]]
-	v = [1, -2, -1, 1]
-	x = [3, 2, 1]
-	y = 2
-	eta = 0.01
-	use_dropout = True
+	w, v = xaviar_initalization()
+	w2, v2 = constant_initalization(1)
+	dataset = [
+		[1, 1, 0],
+		[-1, 1, 1],
+		[1, -1, 1],
+		[-1, -1, 0]
+	]
+	use_dropout = False
 
-	# provide a node dropout list, instead of randomly dropping nodes
-	# only used for testing purposes, True = drop, False = dont drop
-	# dropped_nodes_all = [[False, False, False], [False, True, False], [True, False, False]]
-	dropped_nodes_all = []
+	print("----  INPUT ----")
+	print("dataset = {}").format(dataset)
+	print("w = {}").format(w)
+	print("v = {}").format(v)
+	print("use dropout = {}\n").format(use_dropout)
 
-	# do task 2.b
-	# start with default w and v
-	w_new = w
-	v_new = v
-	y_hat = 0
-	for i in range(3):
-		print("ITERATION {}: ").format(i+1)
+	#x_axis, y_axis = rms_prop(w, v, dataset, 0.2, 0.5, 20)
+	#del x_axis[0:2]
+	#del y_axis[0:2]
+	x_axis2, y_axis2 = rms_prop(w2, v2, dataset, 0.2, 0.5, 100)
 
-		if not dropped_nodes_all:
-			v_new, w_new, y_hat = iteration_step(w_new, v_new, x, y, eta, use_dropout)
-		else:
-			v_new, w_new, y_hat = iteration_step(w_new, v_new, x, y, eta, use_dropout, dropped_nodes_all[i])
+	#plt.plot(x_axis, y_axis, 'rx')
+	plt.plot(x_axis2, y_axis2, 'bx')
 
-		print("w_new: {}").format(v_new)
-		print("v_new: {}").format(w_new)
-
-		y_hat = forward_computation(w_new, v_new, x)[1]
-		print("loss with new w, v: {}").format(loss(y, y_hat))
-		print("END OF ITERATION {}: ").format(i+1)
-
-	# do task 2.a
-	x_new = [1,3,1]
-	forward_computation(w_new, v_new, x_new)[1]
-
+	plt.ylabel('total loss')
+	plt.xlabel('epoch')
+	plt.show()
 
 if __name__ == '__main__':
 	main()
