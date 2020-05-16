fa5e5ca26713c5b07c027acef10c045a13194030
Jodscho <Jonathan2008@gmx.de>
2020-05-17 00:26:23 +0200

    ex-02: bug fix: dont override dataset
    * and use correct mean for w

--- dl_ex-02-2ab.py
+++ dl_ex-02-2ab.py
@@ -18,8 +18,8 @@ def loss(y, y_hat):
 def loss_der(y, y_hat):
 	loss = 0
 	for i in range(len(y_hat)):
-		loss += -2 * (y[i] - y_hat[i])
-	return loss
+		loss += (y[i] - y_hat[i])
+	return -2*loss
 
 def init_array(i, j = 0):
 	if j is 0:
@@ -59,7 +59,7 @@ def xaviar_initalization():
 	for i in range(2):
 		w.append([])
 		for j in range(2):
-			w[i].append(r.normal(loc=0, scale=1/0.5))
+			w[i].append(r.normal(loc=0, scale=0.5))
 		w[i].append(0) # for the bias
 
 	return w, v
@@ -73,10 +73,17 @@ def rms_prop(w, v, dataset, delta, roh, iterations):
 	r_w = init_array(len(w), len(w[0]))
 	mu = 0.1 # no mu given in task description
 
-	for epoch in range(iterations):
+	for epoch in range(20):
+
+		print("--- START EPOCH {} ----").format(epoch)
+
 		x_axis.append(epoch)
 
 		gradient_w, gradient_v = backpropagation_dataset(v, w, dataset, y_axis)
+		#print("gradient_w: {}").format(gradient_w)
+		#print("gradient_v: {}").format(gradient_v)
+
+
 
 		for i in range(len(v)):
 			r_v[i] = roh * r_v[i] + (1 - roh)*(v[i] * v[i])
@@ -88,11 +95,14 @@ def rms_prop(w, v, dataset, delta, roh, iterations):
 				r_w[i][j] = roh * r_w[i][j] + (1 - roh)*(w[i][j] * w[i][j])
 				mu_tilde = mu / (delta + math.sqrt(r_w[i][j]))
 				w[i][j] = w[i][j] - mu_tilde * gradient_w[i][j]
+		print("--- END EPOCH {} ----").format(epoch)
+
 		
 	#return v, w
 
-	print("v = {}").format(v)
-	print("w = {}").format(w)
+	#print("v = {}").format(v)
+	#print("w = {}").format(w)
+
 	return x_axis, y_axis
 
 
@@ -100,14 +110,22 @@ def backpropagation_dataset(v, w, dataset, y_axis):
 	"""Calculates the gradients over the entire training set.
 	"""
 
-	y = [i[2] for i in dataset] # get all the y-values from the dataset as a vector
+	# get all the y-values from the dataset as a vector
+	y = []
+	for i in range(len(dataset)):
+		y.append(dataset[i][2])
+
 	y_hat_all = forward_computation_dataset(w, v, dataset) # compute prediction for every input
 	y_axis.append(loss(y, y_hat_all))
 
-	print("total loss: {}").format(loss(y, y_hat_all))
+	#print("total loss: {}").format(loss(y, y_hat_all))
 
 	# calculate the derivative loss for the backpropagation
 	loss_der_now = loss_der(y, y_hat_all)
+	print("LOSS DER : {}").format(loss_der_now)
+
+	print("using y = {} and y_hat = {}").format(y, y_hat_all)
+	print("using w = {} and v = {}").format(w, v)
 
 	# contains the gradients over all training elements
 	gradient_v_all = init_array(len(v))
@@ -116,7 +134,7 @@ def backpropagation_dataset(v, w, dataset, y_axis):
 	# call the backpropagation method for every element in the training set
 	for i in range(len(dataset)):
 		# get the current x from the dataset
-		x = dataset[i]
+		x = list(dataset[i])
 		x[len(x)-1] = 1 # override y to be the bias 1
 
 		# we need the activation output for backprop
@@ -150,11 +168,12 @@ def backpropagation_dataset(v, w, dataset, y_axis):
 def forward_computation_dataset(w, v, dataset):
 	"""Calculates the predictions for the entire training set.
 	"""
+	print("--- FOWARD COMP ----")
 
 	y_hat_all = []
 
 	for i in range(len(dataset)):
-		x = dataset[i]
+		x = list(dataset[i])
 		x[len(x)-1] = 1 # override y to be the bias 1
 		y_hat_all.append(forward_computation(w, v, x)[1])
 
@@ -191,6 +210,8 @@ def backpropagation(v, w, x, h_l1, loss_der_now, dropped_nodes = []):
 	one has to call this method for every training example in the training dataset.
 	"""
 
+	print("---- START: BACKPROPAGATION ----")
+
 	# first calculate the gradient for v
 	gradient_v = init_array(len(v))
 	for i in range(len(v)):
@@ -198,6 +219,8 @@ def backpropagation(v, w, x, h_l1, loss_der_now, dropped_nodes = []):
 		if dropped_nodes and dropped_nodes[i]:
 			continue
 		gradient_v[i] = loss_der_now * h_l1[i]
+		print("gradient of v_{} = {} * {} = {}").\
+		format(i, loss_der_now, h_l1[i], gradient_v[i])
 
 	# then calculate the gradient for w
 	gradient_w = init_array(len(w), len(w[0]))
@@ -207,6 +230,11 @@ def backpropagation(v, w, x, h_l1, loss_der_now, dropped_nodes = []):
 			continue
 		for j in range(len(w[0])):
 			gradient_w[i][j] = x[j] * relu_der(h_l1[i]) * v[i] * loss_der_now
+			print("gradient of w_{}{} = {} * {} * {} {} = {}").\
+			format(i, j, x[j], relu_der(h_l1[i]), v[i], loss_der_now, gradient_w[i][j])
+
+	print("---- END: BACKPROPAGATION ----")
+
 
 
 	return gradient_v, gradient_w
@@ -230,13 +258,17 @@ def main():
 	print("v = {}").format(v)
 	print("use dropout = {}\n").format(use_dropout)
 
-	#x_axis, y_axis = rms_prop(w, v, dataset, 0.2, 0.5, 20)
+	#x_axis2, y_axis2 = rms_prop(w2, v2, dataset, 0.2, 0.5, 50)
+	x_axis, y_axis = rms_prop(w, v, dataset, 0.2, 0.5, 50)
+	#while y_axis[1] > 5:
+	#	x_axis, y_axis = rms_prop(w, v, dataset, 0.2, 0.5, 50)
+
+
 	#del x_axis[0:2]
 	#del y_axis[0:2]
-	x_axis2, y_axis2 = rms_prop(w2, v2, dataset, 0.2, 0.5, 100)
 
-	#plt.plot(x_axis, y_axis, 'rx')
-	plt.plot(x_axis2, y_axis2, 'bx')
+	plt.plot(x_axis, y_axis, 'rx')
+	#plt.plot(x_axis2, y_axis2, 'bx')
 
 	plt.ylabel('total loss')
 	plt.xlabel('epoch')
